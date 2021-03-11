4d486c8a8f1259c6007aa19fc9f24d9dc87a9449
afoste53 <74116498+afoste53@users.noreply.github.com>
2021-03-10 19:22:18 -0500

    Update Homescreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,66 @@
-const HomeScreen = (props) => {
+import "../App.css";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+
+const HomeScreen = ({ data }) => {
+  const { id, typeEnum, firstName, lastName, courses } = data;
+  console.log(data);
   return (
-    <div>
-      <h1>Hey there, this is a homescreen</h1>
-    </div>
+    <Container>
+      <div className="mr-auto mt-3 mb-5">
+        <h1>Welcome Back {firstName}!</h1>
+      </div>
+      <br />
+
+      <Container className="my-3">
+        <Row>
+          <Col>
+            <Card className="landingPageCard">
+              <Card.Header className="bg-secondary">
+                <Card.Title className="landingPageCardTitle">
+                  <span>
+                    <i className="fas fa-chart-bar fa-1.5x mr-2"></i>{" "}
+                  </span>
+                  Explore Simulations
+                </Card.Title>
+              </Card.Header>
+              <Card.Body></Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card className="landingPageCard text-weight-bold">
+              <Card.Header className="bg-secondary">
+                <Card.Title className="landingPageCardTitle">
+                  <span>
+                    <i className="fas fa-calendar-alt fa-1.7x mr-2"></i>{" "}
+                  </span>
+                  My Courses
+                </Card.Title>
+              </Card.Header>
+              <Card.Body>
+                <ul className="courseList list-unstyled">
+                  {courses.map((course) => (
+                    <li>{course}</li>
+                  ))}
+                </ul>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card className="landingPageCard">
+              <Card.Header className="bg-secondary">
+                <Card.Title className="landingPageCardTitle">
+                  <span>
+                    <i className="fas fa-user fa-1.5x mr-2"></i>{" "}
+                  </span>
+                  My Profile
+                </Card.Title>
+              </Card.Header>
+              <Card.Body></Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
   );
 };
 
