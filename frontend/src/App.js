d4879d6ba30ca21c89db1535d8974f0f8faaed34
ross-andrew-rucho <59429093+ross-andrew-rucho@users.noreply.github.com>
2021-03-31 15:40:42 -0400

    Security Issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,19 +20,6 @@ import "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA1gu3Z0AhuGUQXFMOl9C69-V-eCcwD3hI",
-  //authDomain: "unc-physics-simulation.firebaseapp.com",
-  //databaseURL: "https://unc-physics-simulation-default-rtdb.firebaseio.com",
-  databaseURL: "https://localhost:8080",
-  projectId: "unc-physics-simulation",
-  //storageBucket: "unc-physics-simulation.appspot.com",
-  //messagingSenderId: "188932414514",
-
-  //appId: "1:188932414514:web:c7df0cb566929883fc302b",
-  //measurementId: "G-Z97JLPVR8C"
-};
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 //firebase.analytics();
