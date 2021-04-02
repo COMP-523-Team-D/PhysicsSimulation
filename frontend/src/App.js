a4e8a012585677b6a21258a9cb691b68c8262d68
ross-andrew-rucho <59429093+ross-andrew-rucho@users.noreply.github.com>
2021-04-02 19:25:28 -0400

    Most Recent App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,42 +13,6 @@ import * as ROUTES from "./constants/routes";
 import { withAuthentication } from "./Session";
 import "./App.css";
 
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-import firebase from "firebase/app";
-
-// Add the Firebase services that you want to use
-import "firebase/auth";
-import "firebase/firestore";
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA1gu3Z0AhuGUQXFMOl9C69-V-eCcwD3hI",
-  authDomain: "unc-physics-simulation.firebaseapp.com",
-  databaseURL: "https://unc-physics-simulation-default-rtdb.firebaseio.com",
-  projectId: "unc-physics-simulation",
-  storageBucket: "unc-physics-simulation.appspot.com",
-  messagingSenderId: "188932414514",
-  appId: "1:188932414514:web:c7df0cb566929883fc302b",
-  measurementId: "G-Z97JLPVR8C",
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-//firebase.analytics();
-
-/*
- * Firebase Authentication
- * & Firebase Firestore
- */
-const auth = firebase.auth();
-auth.useEmulator("http://localhost:9099"); // Initialize to use the emulators
-
-const db = firebase.firestore();
-db.useEmulator("localhost", 8080); // Initialize to use the emulators
->>>>>>> 79310aeef1c18ac5a9b7c39686c999a9f7e97bc8
-
 // main container component
 
 // example dummy data
