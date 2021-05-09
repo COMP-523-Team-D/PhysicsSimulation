4fd9d0f56da89c702951d05e8baa65e540d0107a
Gabe <mrginch@gmail.com>
2021-05-09 17:21:52 -0400

    add help screen to nav

--- frontend/src/constants/routes.js
+++ frontend/src/constants/routes.js
@@ -1,21 +1,22 @@
 /**
  * Repository for the static routes used
  * by the application.
- * 
+ *
  * Date: 05/12/2021
  * @author Ross Rucho
  */
 
-export const LANDING_SCREEN = '/';
-export const HOME_SCREEN = '/home';
-export const REGISTER_SCREEN = '/register';
-export const LOGIN_SCREEN = '/login';
-export const PROBLEM_SCREEN = '/problem';
-export const SUBMISSION_SCREEN = '/submission';
-export const STUDENTS_SCREEN = '/students';
-export const STUDENT_SUBMISSIONS_SCREEN = '/student_submissions';
-export const GRADE_SCREEN = '/grade';
-export const BUILD_SCREEN = '/build';
-export const SANDBOX_SCREEN = '/sandbox';
-export const COURSE_SCREEN = '/course';
-export const ASSIGNMENT_SCREEN = '/assignment';
+export const LANDING_SCREEN = "/";
+export const HOME_SCREEN = "/home";
+export const REGISTER_SCREEN = "/register";
+export const LOGIN_SCREEN = "/login";
+export const PROBLEM_SCREEN = "/problem";
+export const SUBMISSION_SCREEN = "/submission";
+export const STUDENTS_SCREEN = "/students";
+export const STUDENT_SUBMISSIONS_SCREEN = "/student_submissions";
+export const GRADE_SCREEN = "/grade";
+export const BUILD_SCREEN = "/build";
+export const SANDBOX_SCREEN = "/sandbox";
+export const COURSE_SCREEN = "/course";
+export const ASSIGNMENT_SCREEN = "/assignment";
+export const HELP_SCREEN = "/help";
---
+++ frontend/src/screens/HelpScreen.js
@@ -0,0 +1,9 @@
+const HelpScreen = (props) => {
+  return (
+    <div>
+      <h1>OH DEAR GOD SOMEBODY HELP ME</h1>
+    </div>
+  );
+};
+
+export default HelpScreen;
