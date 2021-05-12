8b41ad06920775abc8d308547548f254c10b107e
Gabe <mrginch@gmail.com>
2021-05-12 11:38:55 -0400

    fix help screen responsiveness

--- frontend/src/screens/HelpScreen.js
+++ frontend/src/screens/HelpScreen.js
@@ -1,4 +1,4 @@
-import { Button, Col, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import GraphComponent from "../components/GraphComponent";
 import { useState } from "react";
@@ -11,21 +11,22 @@ const HelpScreen = (props) => {
   const [graphAnswers, getGraphAnswers] = useState({});
 
   return (
-    <div as={Col} className="help-screen-col mb-5">
-      <section id="help-section-1" as={Col} className="help-section-1 mb-5">
-        <h2 className="m-0">Registering A New User</h2>
+    <Col className="help-screen-col p-auto my-2">
+      <section id="help-section-1" className="help-section-1">
+        <h2 className="">Registering A New User</h2>
         <br />
-        <Row className="m-4 d-flex justify-content-around align-items-center">
-          <Col className="mr-auto" sm={5}>
+        <Row className="">
+          <Col xs={12} lg={8} className="d-flex align-items-center">
             <img
               src={registerStudentImg}
+              id="help-screen-img-1"
               className="help-screen-img"
-              alt="a picture"
+              alt="Registering a new user"
             />
           </Col>
-          <Col className="ml-auto" sm={5}>
-            <Row className="my-3">
-              <h3 className="h4">Student</h3>
+          <Col xs={12} lg={4} className="">
+            <Row className="">
+              <h3 className="">Student</h3>
               <p>
                 In order to register yourself as a new student, navigate to the
                 register page using the tab in the menu bar. You will then be
@@ -39,7 +40,7 @@ const HelpScreen = (props) => {
               </small>
             </Row>
             <Row>
-              <h3 className="h4">Instructor</h3>
+              <h3 className="">Instructor</h3>
               <p>
                 To register yourself as a new instructor, follow the same steps
                 as above but do <strong>not</strong> select an instructor from
@@ -52,7 +53,7 @@ const HelpScreen = (props) => {
         </Row>
         <br />
         <Row className="d-flex justify-content-center">
-          <small>
+          <small className="">
             Not ready to register? Feel free to{" "}
             <strong>
               <Link to="/">check out</Link>
@@ -66,68 +67,72 @@ const HelpScreen = (props) => {
 
       <br />
 
-      <section id="help-section-2" as={Col} className="help-col">
-        <h2 className="m-0">Graphing a Prediction</h2>
+      <section id="help-section-2" className="help-col">
+        <h2 className="">Graphing a Prediction</h2>
         <br />
-        <Row className="m-4 d-flex justify-content-around align-items-center">
-          <p>
-            As part of an assignment you may be asked to plot points on a graph.
-            This is a little confusing at first but not too bad once you've done
-            it a couple of times.
-          </p>
-          <Col className="mr-1" sm={5}>
+        <div className="col-12 row-md">
+          <Row>
             <p>
-              In order to a graph a single point all you need to do is click the
-              point you'd like to graph.
+              As part of an assignment you may be asked to plot points on a
+              graph. This is a little confusing at first but not too bad once
+              you've done it a couple of times.
             </p>
-            <p>
-              In order to graph a straight line, click two points, with distinct
-              x-values, that the line will run through.
-            </p>
-            <p>To graph a parabola, select three points on the curve.</p>
-            <p>
-              Feel free to play around with the one here until you get the hang
-              of it.
-            </p>
-          </Col>
-          <Col className="ml-1" sm={5}>
-            <GraphComponent
-              ind={null}
-              dep={null}
-              xMin={0}
-              xMax={25}
-              yMin={0}
-              yMax={25}
-              name="Practice Graph"
-              handleAnswers={getGraphAnswers}
-              answersToSet={[]}
-            />
-          </Col>
-        </Row>
+          </Row>
+          <Row>
+            <Col className="col-12 col-md-6 ">
+              <p>
+                In order to a graph a single point all you need to do is click
+                the point you'd like to graph.
+              </p>
+              <p>
+                In order to graph a straight line, click two points, with
+                distinct x-values, that the line will run through.
+              </p>
+              <p>To graph a parabola, select three points on the curve.</p>
+              <p>
+                Feel free to play around with the one here until you get the
+                hang of it.
+              </p>
+            </Col>
+            <Col className="col-12 col-md-6">
+              <GraphComponent
+                ind={null}
+                dep={null}
+                xMin={0}
+                xMax={25}
+                yMin={0}
+                yMax={25}
+                name="Practice Graph"
+                handleAnswers={getGraphAnswers}
+                answersToSet={[]}
+              />
+            </Col>
+          </Row>
+        </div>
       </section>
 
       <br />
 
-      <section id="help-section-3" as={Col} className="mb-5">
-        <h2 className="m-0">Creating An Assignment</h2>
+      <section id="help-section-3" className="">
+        <h2 className="">Creating An Assignment</h2>
         <br />
-        <Row className="m-4 d-flex justify-content-around align-items-center">
-          <Col className="mr-auto" sm={4} lg={5}>
+        <Row className="my-3">
+          <Col className="d-flex align-items-center">
             <img
               src={createAssignmentImg1}
               className="help-screen-img"
-              alt="a picture"
+              alt="Creating an assignment"
             />
           </Col>
-          <Col className="ml-auto" sm={4} lg={5}>
-            <Row className="my-3">
+          <Col className="align-self-center">
+            <Row>
               <p>
                 After you are logged in and looking at your class home page, you
                 should see any assignments you've already created above, and a
                 button to create a new assignment below.
               </p>
             </Row>
-            <Row>
+            <Row className="">
               <p>
                 At this point you should see a form with the Course Name and
                 Assignment Name already filled out. After you've set the release
@@ -144,37 +149,44 @@ const HelpScreen = (props) => {
           </Col>
         </Row>
 
-        <Row className="m-4 d-flex justify-content-around align-items-center">
-          <Col className="mr-1 my-4" sm={5}>
-            <p>
-              On the pages that follow you will be prompted to fill out a form
-              for each question you wish to include as part of this assingment.
-            </p>
-            <p>
-              You will need to select which simulation the problem will be
-              dealing with, what parameters to fix, how many sub-questions you'd
-              like to include, the text for those questions, how many graphs
-              you'd like the students to do, and which variables you'd like the
-              students to graph.
-            </p>
-            <p>
-              After you've filled out the info you'd like to include for this
-              assignment you will need to navigate back to the first screen,
-              confirming you have added everything the way you want it along the
-              way, and click on the 'Create Assignment' button. The assignment
-              should now show up under its respective class.
-            </p>
+        <Row className="my-3">
+          <Col className="align-self-center">
+            <Row className="">
+              <p>
+                On the pages that follow you will be prompted to fill out a form
+                for each question you wish to include as part of this
+                assignment.
+              </p>
+            </Row>
+            <Row>
+              <p>
+                You will need to select which simulation the problem will be
+                dealing with, what parameters to fix, how many sub-questions
+                you'd like to include, the text for those questions, how many
+                graphs you'd like the students to do, and which variables you'd
+                like the students to graph.
+              </p>
+            </Row>
+            <Row>
+              <p>
+                After you've filled out the info you'd like to include for this
+                assignment you will need to navigate back to the first screen,
+                confirming you have added everything the way you want it along
+                the way, and click on the 'Create Assignment' button. The
+                assignment should now show up under its respective class.
+              </p>
+            </Row>
           </Col>
-          <Col className="ml-1 my-4" sm={5}>
+          <Col className="align-self-center">
             <img
               src={createAssignmentImg2}
               className="help-screen-img"
-              alt="a picture"
+              alt="Continuing to create an assignment"
             />
           </Col>
         </Row>
       </section>
-    </div>
+    </Col>
   );
 };
 
